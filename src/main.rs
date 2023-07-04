1f94236edeeb3127c486138a589f6f4c7985cef4
ahsan-javaid <ahsan786cs@gmail.com>
2023-07-04 18:23:40 +0500

    feat: generate a peer key

---
+++ src/main.rs
@@ -0,0 +1,13 @@
+use libp2p::{identity, PeerId};
+use std::error::Error; 
+
+#[async_std::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    // Generate a key pair
+    let local_key = identity::Keypair::generate_ed25519();
+    println!("{local_key:?}");
+    let local_peer_id = PeerId::from(local_key.public());
+    println!("Local peer id: {local_peer_id:?}");
+
+    Ok(())
+}
